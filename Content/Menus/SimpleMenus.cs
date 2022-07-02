6f7a706da605e164f5be4da9dce14245a6d55e72
Blockaroz <lbpk.123fan@gmail.com>
2022-07-02 18:37:02 -0400

    update

--- Content/Menus/SimpleMenus.cs
+++ Content/Menus/SimpleMenus.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using System;
 using Terraria;
+using Terraria.GameContent.Events;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -39,6 +40,8 @@ namespace MoreMenus.Content
 
         public override Asset<Texture2D> Logo => Mod.Assets.Request<Texture2D>("Assets/General/JungleLogo");
 
+        public override int Music => MusicID.Jungle;
+
         public override bool PreDrawLogo(SpriteBatch spriteBatch, ref Vector2 logoDrawCenter, ref float logoRotation, ref float logoScale, ref Color drawColor)
         {
             if (ModContent.GetInstance<MenuConfig>().MenuOnLeft == true)
@@ -56,6 +59,8 @@ namespace MoreMenus.Content
     public class HallowLogo : ModMenu
     {
         public override string DisplayName => "Hallow";
+
+        public override int Music => MusicID.TheHallow;
 
         public override Asset<Texture2D> Logo => Mod.Assets.Request<Texture2D>("Assets/General/HallowLogo");
 
@@ -79,6 +84,8 @@ namespace MoreMenus.Content
 
         public override Asset<Texture2D> Logo => Mod.Assets.Request<Texture2D>("Assets/General/MushroomLogo");
 
+        public override int Music => MusicID.Mushrooms;
+
         public override void Update(bool isOnTitleScreen)
         {
             Main.SmoothedMushroomLightInfluence = MathHelper.Lerp(Main.SmoothedMushroomLightInfluence, 1f, 0.2f);
@@ -99,6 +106,26 @@ namespace MoreMenus.Content
             darkBlue.A = 0;
 
             spriteBatch.Draw(glow.Value, logoDrawCenter, null, darkBlue, logoRotation, glow.Size() * 0.5f, logoScale, 0, 0);
+        }
+    }
+
+    public class PurityBrightLogo : ModMenu
+    {
+        public override string DisplayName => "Bright Forest";
+
+        public override Asset<Texture2D> Logo => Mod.Assets.Request<Texture2D>("Assets/General/PurityBrightLogo");
+
+        public override bool PreDrawLogo(SpriteBatch spriteBatch, ref Vector2 logoDrawCenter, ref float logoRotation, ref float logoScale, ref Color drawColor)
+        {
+            if (ModContent.GetInstance<MenuConfig>().MenuOnLeft == true)
+                logoDrawCenter = new Vector2(Main.screenWidth / 5f, Main.screenHeight / 2.2f + ((float)Math.Sin(Main.GlobalTimeWrappedHourly / 1.9f) * 16));
+
+            spriteBatch.Draw(Logo.Value, logoDrawCenter, null, drawColor * (Main.LogoA / 255f), logoRotation, Logo.Size() * 0.5f, logoScale, 0, 0);
+
+            Asset<Texture2D> nightLogo = Mod.Assets.Request<Texture2D>("Assets/General/PurityBrightLogoNight");
+            spriteBatch.Draw(nightLogo.Value, logoDrawCenter, null, drawColor * (Main.LogoB / 255f), logoRotation, nightLogo.Size() * 0.5f, logoScale, 0, 0);
+
+            return false;
         }
     }
 }
